20728b68b1732bc15468aa34d3a50d9f5d626e3a
Gustavo Giráldez <gustavo.giraldez@gmail.com>
2012-08-26 21:01:03 -0300

    Improve build batch

--- make.bat
+++ make.bat
@@ -3,7 +3,9 @@ set LOVEPREFIX="c:\program files (x86)\love"
 rmdir /s /q exe
 mkdir exe
 7z a exe\ld24.zip *.lua README assets\*.png assets\*.ttf assets\*.wav
-copy /b %LOVEPREFIX%\love.exe + exe\ld24.zip exe\ld24.exe
+copy /b %LOVEPREFIX%\love.exe + exe\ld24.zip exe\pincers.exe
 copy %LOVEPREFIX%\*.dll exe
 del exe\ld24.zip
+cd exe
+7z a pincers.zip pincers.exe *.dll
 
